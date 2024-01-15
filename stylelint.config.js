162aa9e47d86f3e6acc39e7ed34c03b1e18236c8
Adam Hellberg <sharparam@sharparam.com>
2024-01-15 21:55:49 +0100

    Allow monospace duplicate in font names

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,10 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard"],
+  rules: {
+    "font-family-no-duplicate-names": [
+      true,
+      { ignoreFontFamilyNames: ["monospace"] }
+    ]
+  }
 };
