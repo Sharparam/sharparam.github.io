0f7760d92fd9d2e6dab7488913363be7df5c27ef
Adam Hellberg <sharparam@sharparam.com>
2024-01-16 02:15:00 +0100

    Update stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@
 export default {
   extends: ["stylelint-config-standard"],
   rules: {
+    "comment-empty-line-before": false,
     "font-family-no-duplicate-names": [
       true,
       { ignoreFontFamilyNames: ["monospace"] }
